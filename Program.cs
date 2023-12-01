3171772217cd610410212b38ce6797571ad1b336
Jose Agraz <joseagrazm@gmail.com>
2023-12-01 23:56:46 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Factorial_de_un_numero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial_recursivo(5));
+        }
+
+        static int Factorial_recursivo(int num)
+        {
+            if (num == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return num * Factorial_recursivo(num-1);
+            }
+        }
+    }
+}
